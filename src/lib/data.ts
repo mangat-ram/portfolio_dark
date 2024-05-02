2ab05a3bd3bd3e2fd6b2fc2dca1e0a8facca55c9
mangat-ram <se.mangat.ram@gmail.com>
2024-05-02 21:04:55 +0530

    added Data File.

---
+++ src/lib/data.ts
@@ -0,0 +1,23 @@
+type SocialsArrType = {
+  link: string;
+  label: string;
+  icon: string;
+}
+
+export const SocialsArr: Array<SocialsArrType> = [
+  {
+    link:"www.linkedin.com/in/mangat-ram-dev",
+    label:"LinkedIn",
+    icon:""
+  },
+  {
+    link:"https://github.com/mangat-ram",
+    label:"GitHub",
+    icon:""
+  },
+  {
+    link:"{twitterUrl}",
+    label:"Twitter",
+    icon:""
+  }
+]

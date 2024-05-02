7f9dda84f6d1982a6f6ad99adcbb813fc78d7a32
mangat-ram <se.mangat.ram@gmail.com>
2024-05-02 20:44:43 +0530

    Added Page Of Home

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import NavBar from "./components/NavBar"
+
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-black">
+      <div className="max-w-7xl mx-auto">
+        <NavBar />
+      </div>
+    </div>
+  )
+}

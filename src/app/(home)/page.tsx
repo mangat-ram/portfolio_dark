bbd2f2e9fe61c1f5abfb124e5f8d7f92f18b4697
mangat-ram <se.mangat.ram@gmail.com>
2024-05-03 14:29:20 +0530

    Added the Background.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import HeroSection from "./components/HeroSection"
 export default function Page() {
   return (
     <div className="min-h-screen bg-black overflow-hidden">
-      <div className="max-w-7xl mx-auto p-5">
+      <div className="max-w-7xl mx-auto p-5 dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2]">
         <NavBar />
         <HeroSection />
       </div>
